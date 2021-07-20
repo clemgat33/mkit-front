25cbe75655d22ce99968b4f867c8430963d8eedb
cidgi359 <74004967+cidgi359@users.noreply.github.com>
2021-07-20 21:04:00 +0300

    added route search page

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './styles/global.scss';
 
-import Home from './pages/HomePage';
+import HomePage from './pages/HomePage';
+import SearchPage from './pages/SearchPage';
 
 export default function App(): JSX.Element {
 	return (
 		<Switch>
-			<Route exact path="/" component={Home} />
+			<Route exact path="/" component={HomePage} />
+			<Route path="/search" component={SearchPage} />
 		</Switch>
 	);
 }
