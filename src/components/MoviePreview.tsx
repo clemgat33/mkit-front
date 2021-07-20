faf7849fd08521b0d2f8c59daacf6a298040668a
cidgi359 <74004967+cidgi359@users.noreply.github.com>
2021-07-20 21:19:32 +0300

    added search page & movie previews

---
+++ src/components/MoviePreview.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import styles from '@/styles/modules/components/MoviePreview.module.scss';
+
+import { MoviePreviewProps } from '@/interfaces';
+type PropsComponent = {
+	content: MoviePreviewProps
+}
+
+export default function MoviePreview({content}: PropsComponent): JSX.Element{
+
+	const genres = content.genres.map((genre, key) => {
+		// add ', ' to each genre except the last one
+		 if(key+1 !== content.genres.length) return genre + ', ';
+		 else  return genre;
+	});
+
+	function handleFavorite(){
+		console.log('FAVORITE',content);
+	}
+
+	return(
+		<div className={styles.container}>
+			<Link to='/' >
+				<div className={styles.image}>
+					<img
+						src={content.image_url}
+						alt='image-movie'
+						width={200}
+						height={300}
+					/>
+
+				</div>
+			</Link>
+			<div className={styles.wrapper}>
+				<div className={styles.wrapper_top}>
+					<h1>{content.title}</h1>
+					<h5>{genres} | {content.runtime} minutes</h5>
+					<div dangerouslySetInnerHTML={{ __html: content.summary}} />
+					<a href={content.url} className='link'>Visit official site</a>
+				</div>
+				<button className='btn--secondary' onClick={handleFavorite}>Add To Favorites</button>
+			</div>
+		</div>
+	);
+}
---
+++ src/pages/SearchPage.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+
+import Layout from '@/Layout';
+import SearchBar from '@/components/SearchBar';
+import MoviePreview from '@/components/MoviePreview';
+
+import styles from '@/styles/modules/pages/SearchPage.module.scss';
+
+
+import { Meta } from '@/interfaces';
+export function getMeta(): Meta{
+	return {
+		title: 'Search',
+		description: 'Search',
+		og_title: 'Search',
+		og_description: 'Search',
+	};
+}
+
+
+export default function SearchPage(): JSX.Element {
+
+	const content = {
+		title: 'Under the Dome',
+		image_url: 'https://static.tvmaze.com/uploads/images/medium_portrait/81/202627.jpg',
+		date:  '2013-06-24',
+		genres: [
+			'Drama',
+			'Science-Fiction',
+			'Thriller'
+		],
+		runtime: 60,
+		summary: '<p><b>Under the Dome</b> is the story of a small town that is suddenly and inexplicably sealed off from the rest of the world by an enormous transparent dome. The town\'s inhabitants must deal with surviving the post-apocalyptic conditions while searching for answers about the dome, where it came from and if and when it will go away.</p>',
+		url: 'https://www.tvmaze.com/shows/1/under-the-dome'
+	};
+
+	return(
+		<Layout meta={getMeta()}>
+			<div className='section'>
+				<h1 className='section--title'>Search</h1>
+				<div className='center'>
+					<SearchBar />
+				</div>
+				<div className={styles.wrapper}>
+					<MoviePreview content={content}/>
+					<MoviePreview content={content}/>
+					<MoviePreview content={content}/>
+ 				</div>
+			</div>
+		</Layout>
+	);
+}
