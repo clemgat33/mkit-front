b87a97bee7f43ac562271f50e1321534187ca4a7
cidgi359 <74004967+cidgi359@users.noreply.github.com>
2021-07-21 23:54:20 +0300

    scroll to top changing route

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './styles/global.scss';
 
@@ -6,12 +6,24 @@ import HomePage from './pages/HomePage';
 import SearchPage from './pages/SearchPage';
 import MoviePage from './pages/movies/MoviePage';
 
+import { useLocation } from 'react-router-dom';
+function ScrollToTop() {
+	const { pathname } = useLocation();
+	useEffect(() => {
+		window.scrollTo(0, 0);
+	}, [pathname]);
+	return null;
+}
+
 export default function App(): JSX.Element {
 	return (
-		<Switch>
-			<Route exact path="/" component={HomePage} />
-			<Route path="/search" component={SearchPage} />
-			<Route path="/movies/:movie_id" component={MoviePage} />
-		</Switch>
+		<>
+			<ScrollToTop/>
+			<Switch>
+				<Route exact path="/" component={HomePage} />
+				<Route path="/search" component={SearchPage} />
+				<Route path="/movies/:movie_id" component={MoviePage} />
+			</Switch>
+		</>
 	);
 }
