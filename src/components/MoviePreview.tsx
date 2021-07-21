12b1d7eb6c02d09c7a9b8ca5b0bdd5155f93e58a
cidgi359 <74004967+cidgi359@users.noreply.github.com>
2021-07-21 23:26:06 +0300

    create elements in case some data doesnt exist

--- src/components/MoviePreview.tsx
+++ src/components/MoviePreview.tsx
@@ -23,35 +23,53 @@ export default function MoviePreview({content, isSearch}: PropsComponent): JSX.E
 		console.log('FAVORITE',content);
 	}
 
-	const imageElement = (
 
-		<img
-			src={content.image_url || no_thumbnail}
-			alt='image-movie'
-			width={200}
-			height={300}
-		/>
+	/* ELEMENTS */
+	const imageElement = isSearch ? (
+		<Link to={`/movies/${content.movie_id}`} >
+			<div className={`${styles.image} ${styles.image_zoom}`} >
+				<img
+					src={content.image_url || no_thumbnail}
+					alt='image-movie'
+					width={200}
+					height={300}
+				/>
+			</div>
+		</Link>
+	) : (
+		<div className={styles.image}>
+			<img
+				src={content.image_url || no_thumbnail}
+				alt='image-movie'
+				width={200}
+				height={300}
+			/>
+		</div>
 	);
+
+	const year = content.date ? '(' + content.date.slice(0, 4) + ')' : '';
+	const titleElement = isSearch ? (
+		<Link to={`/movies/${content.movie_id}`} >
+			<h1>{content.title} {year}</h1>
+		</Link>
+	) :  <h1>{content.title}</h1>;
+
+	const genresTimeElements = (genres.length > 0 && content.runtime > 0) ? (
+		<h5>{genres} | {content.runtime} minutes</h5>
+	) : genres.length > 0  ? (
+		<h5>{genres}</h5>
+	) : content.runtime > 0 && (
+		<h5>{content.runtime} minutes</h5>
+	);
+	/* ELEMENTS */
 
 	return(
 		<div className={styles.container}>
-			{
-				isSearch ? (
-					<Link to={`/movies/${content.movie_id}`} >
-						<div className={`${styles.image} ${styles.image_zoom}`} >
-							{imageElement}
-						</div>
-					</Link>
-				) :(
-					<div className={styles.image}>
-						{imageElement}
-					</div>
-				)
-			}
+			{imageElement}
 			<div className={styles.wrapper}>
 				<div className={styles.wrapper_top}>
-					<h1>{content.title}</h1>
-					<h5>{genres} | {content.runtime} minutes</h5>
+					{titleElement}
+					{genresTimeElements}
 					<div dangerouslySetInnerHTML={{ __html: content.summary}} />
 					<a href={content.url} className='link' target='_blank' rel="noreferrer">Visit official site</a>
 				</div>
